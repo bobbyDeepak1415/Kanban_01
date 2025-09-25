345f89bf00bd3bbd1778753b3b383c264e11388f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-25 16:58:46 +0530

    added add task function

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 export default function Kanban() {
-  const stageNames = ["Backlogs", "Todo", "Ongoing", "Done"];
+  
+  const [newTask,setNewTask]=useState("")
 
-  const [newTask, setNewTask] = useState("");
+  const stages=["Backlogs","Todo","Ongoing","Done"]
 
-  const [stageTasks, setStageTasks] = useState([[], [], [], []]);
+  const [taskStages,setTaskStages]=useState([[],[],[],[]])
 
-  return <div>Kanban</div>;
+
+  const handleAddTask=(e)=>{
+e.preventDefault()
+if(!newTask.trim()) return
+
+
+const task={name:newTask.trim(),stage:0}
+
+const updatedStages=[...taskStages]
+updatedStages[0].push(task)
+setTaskStages(updatedStages)
+setNewTask("")
+
+
+  }
+
+
+  return (
+    <div className='kanban'>
+      <div className='form_layout'>
+        <form onSubmit={handleAddTask}>
+          <div className='form'>
+
+
+        <input className='task_input' onChange={(e)=>setNewTask(e.target.value)}></input>
+        <button type='submit' className='addTask_btn'>AddTask</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
+
+
