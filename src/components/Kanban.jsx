03db106c2983e48e175610377a38bdfd2d8a44c2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-26 00:13:18 +0530

    start over

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,19 +1,12 @@
-import React, { useState } from "react";
+import React from 'react'
 
 export default function Kanban() {
-  const [newTask, setNewTask] = useState("");
-
-  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
-
-  const [taskStages, setTaskStages] = useState([[], [], [], []]);
+  
 
   
   return (
-    <div className="kanban">
-      <div className="form_layout">
-        <form onSubmit={handleAddTask}></form>
-      </div>
-     
+    <div>
+      Kanban
     </div>
-  );
+  )
 }
