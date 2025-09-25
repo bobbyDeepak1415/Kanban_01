3d4b016ea4d40555683a4a502eaf18a976715dd0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-26 02:35:44 +0530

    added btns and the functionality

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -11,20 +11,41 @@ export default function Kanban() {
     e.preventDefault();
     if (!newTask.trim()) return;
 
+    const updatedStages = [...taskStages];
+
     const task = { name: newTask.trim(), stage: 0 };
 
-    const updatedStages = [...taskStages];
     updatedStages[0].push(task);
     setTaskStages(updatedStages);
     setNewTask("");
   };
+
+  const moveTask = (stageIndex, taskIndex, direction) => {
+    const updatedStages = [...taskStages];
+    const taskToMove = updatedStages[stageIndex][taskIndex];
+
+    updatedStages[stageIndex].splice(taskIndex, 1);
+    updatedStages[stageIndex + direction].push(taskToMove);
+    setTaskStages(updatedStages);
+  };
+
+  const handleDelete=(stageIndex,taskIndex)=>{
+    const updatedStages=[...taskStages]
+
+    updatedStages[stageIndex].splice(taskIndex,1)
+    setTaskStages(updatedStages)
+
+
+
+  }
 
   return (
     <div className="kanban">
       <div className="form_layout">
         <form onSubmit={handleAddTask}>
           <div className="form">
-            <input value={newTask}
+            <input
+              value={newTask}
               className="form_input"
               onChange={(e) => setNewTask(e.target.value)}
             ></input>
@@ -34,15 +55,32 @@ export default function Kanban() {
       </div>
 
       <div className="containers_layout">
-        {stages.map((stage, index) => {
+        {stages.map((stage, stageIndex) => {
           return (
-            <div className="container" key={index}>
+            <div className="container" key={stageIndex}>
               <h4>{stage}</h4>
               <ul className="task_list">
-                {taskStages[index].map((task, taskIndex) => {
+                {taskStages[stageIndex].map((task, taskIndex) => {
                   return (
                     <li key={taskIndex}>
                       <span>{task.name}</span>
+                      <span>
+                        <button
+                          className="back_btn"
+                          onClick={moveTask(stageIndex, taskIndex, -1)}
+                          className="back_btn"
+                        >
+                          ◀
+                        </button>
+                        <button
+                          className="forward_btn"
+                          onClick={moveTask(stageIndex, taskIndex, 1)}
+                          className="forward_btn"
+                        >
+                          ▶
+                        </button>
+                        <button onClick={handleDelete(stageIndex,taskIndex)} className="delete_btn">X</button>
+                      </span>
                     </li>
                   );
                 })}
