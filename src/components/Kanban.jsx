f9a794143d2e0ba920b3b881a1cbf298948c48a7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-25 17:04:30 +0530

    aded the ui part

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,44 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Kanban() {
-  
-  const [newTask,setNewTask]=useState("")
+  const [newTask, setNewTask] = useState("");
 
-  const stages=["Backlogs","Todo","Ongoing","Done"]
+  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
 
-  const [taskStages,setTaskStages]=useState([[],[],[],[]])
+  const [taskStages, setTaskStages] = useState([[], [], [], []]);
 
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return;
 
-  const handleAddTask=(e)=>{
-e.preventDefault()
-if(!newTask.trim()) return
+    const task = { name: newTask.trim(), stage: 0 };
 
-
-const task={name:newTask.trim(),stage:0}
-
-const updatedStages=[...taskStages]
-updatedStages[0].push(task)
-setTaskStages(updatedStages)
-setNewTask("")
-
-
-  }
-
+    const updatedStages = [...taskStages];
+    updatedStages[0].push(task);
+    setTaskStages(updatedStages);
+    setNewTask("");
+  };
 
   return (
-    <div className='kanban'>
-      <div className='form_layout'>
+    <div className="kanban">
+      <div className="form_layout">
         <form onSubmit={handleAddTask}>
-          <div className='form'>
-
-
-        <input className='task_input' onChange={(e)=>setNewTask(e.target.value)}></input>
-        <button type='submit' className='addTask_btn'>AddTask</button>
+          <div className="form">
+            <input
+              className="task_input"
+              onChange={(e) => setNewTask(e.target.value)}
+            ></input>
+            <button type="submit" className="addTask_btn">
+              AddTask
+            </button>
           </div>
         </form>
       </div>
+      <div className="containers_layout">
+        <div className="containers">
+          {taskStages.map((stage, index) => {
+            return (
+              <div className="container" key={index}>
+                <h4>{stage}</h4>
+                <ul className="tasklist">
+                  {taskStages[index].map((task, taskIndex) => {
+                    return (
+                      <li className="taskItem" key={taskIndex}>
+                        <div className="task_Content">
+                          <span>{task.name}</span>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
