ac796ac6806ecff7de55a39f7be07315496df657
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-26 00:23:22 +0530

    added ui and handle add task

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Kanban() {
-  
+  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
 
-  
+  const [newTask, setNewTask] = useState("");
+
+  const [taskStages, setTaskStages] = useState([[], [], [], []]);
+
+  const handleAddTask = (e) => {
+    e.preventDefault()
+    if (!newTask.trim()) return;
+
+    const task = { name: newTask.trim(), stage: 0 };
+
+    const updatedStages = [...taskStages];
+    updatedStages[0].push(task);
+    setTaskStages(updatedStages);
+    setNewTask("");
+  };
+
   return (
-    <div>
-      Kanban
+    <div className="kanban">
+      <div className="form_layout">
+        <form onSubmit={handleAddTask}>
+          <div className="form">
+            <input
+              className="form_input"
+              onChange={(e) => setNewTask(e.target.value)}
+            ></input>
+            <button className="addTask_btn">Add Task</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="containers_layout">
+        {taskStages.map((stage, index) => {
+          return (
+            <div className="container">
+              <h4>{stage}</h4>
+              <ul>
+                {taskStages[index].map((task, taskIndex) => {
+                  return (
+                    <li key={taskIndex}>
+                      <span>{task.name}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
