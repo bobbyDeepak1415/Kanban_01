5d73e80359ba30d1dd29cf4df2747d3eca62ddf6
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-27 00:38:53 +0530

    added everything so far

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Kanban() {
-  
+  const [newTask, setNewTask] = useState("");
+
+  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
+
+  const [taskStages, setTaskStages] = useState([[], [], [], []]);
+
+  const handleAdddTask = (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return;
+
+    const updatedStages = taskStages.map((arr) => [...arr]);
+    const task = { name: newTask.trim(), stage: 0 };
+    updatedStages[0].push(task);
+
+    setTaskStages(updatedStages);
+    setNewTask("");
+  };
+
+  const moveTask = (stageIndex, taskIndex, direction) => {
+    const updatedStages = [...taskStages];
+    const taskToMove = updatedStages[stageIndex][taskIndex];
+
+    updatedStages[stageIndex].splice(taskIndex, 1);
+    updatedStages[stageIndex + direction].push(taskToMove);
+
+    setTaskStages(updatedStages);
+  };
 
   return (
-    <>
+    <div className="kanban">
+      <div className="form_layout">
+        <form onSubmit={handleAdddTask}>
+          <div className="form">
+            <input
+              className="task_input"
+              onChange={(e) => setNewTask(e.target.value)}
+              value={newTask}
+            ></input>
+            <button className="addTask_btn">Add Task</button>
+          </div>
+        </form>
+      </div>
+      <div className="containers">
+        {stages.map((stage, stageIndex) => {
+          return (
+            <div className="container" key={stageIndex}>
+              <h4>{stage}</h4>
+              <ul>
+                {taskStages[stageIndex].map((task, taskIndex) => {
+                  return (
+                    <li key={taskIndex}>
+                      <div>
+                        <span>{task.name}</span>
+                        <span>
+                          <button
+                            disabled={stageIndex === 0}
+                            onClick={() => moveTask(stageIndex, taskIndex, -1)}
+                          >
+                            ←
+                          </button>
+                          <button
+                            disabled={stageIndex === 3}
+                            onClick={() => moveTask(stageIndex, taskIndex, 1)}
+                          >
+                            →
+                          </button>
+                          <button>X</button>
+                        </span>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
       Kanban
-    </>
-  )
+    </div>
+  );
 }
