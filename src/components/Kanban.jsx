436d9580487928e47ab0b41a488b13b10869b5f0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-09-28 15:38:34 +0530

    added the local storage

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Kanban() {
   const [newTask, setNewTask] = useState("");
 
   const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
 
-  const [taskStages, setTaskStages] = useState([[], [], [], []]);
+  // const [taskStages, setTaskStages] = useState([[], [], [], []]);
+
+    const [taskStages, setTaskStages] = useState(()=>{
+      try{
+        const localSaved=localStorage.getItem("kanban-tasks")
+
+        return localSaved ? JSON.parse(localSaved) :[]
+      }catch{
+        console.log("failed fetching data")
+        return []
+      }
+    });
+
+    useEffect(()=>{
+  try{
+    localStorage.setItem("kanban-tasks",JSON.stringify(taskStages))
+  }catch{
+    console.log("failed fetching data")
+    return []
+  }
+    },[taskStages])
 
   const [dragData, setDragData] = useState(null);
-
 
   const handleAddTask = (e) => {
     e.preventDefault();
@@ -38,35 +57,31 @@ export default function Kanban() {
     setTaskStages(updatedStages);
   };
 
-  const handleDragStart=(stageIndex,taskIndex)=>{
-setDragData({stageIndex,taskIndex})
+  const handleDragStart = (stageIndex, taskIndex) => {
+    setDragData({ stageIndex, taskIndex });
+  };
 
-  }
+  const allowDrop = (e) => {
+    e.preventDefault();
+  };
 
-  const allowDrop=(e)=>{
-    e.preventDefault()
-  }
+  const handleDrop = (stageIndex) => {
+    if (!dragData) return;
 
-  const handleDrop=(stageIndex)=>{
+    const updatedStages = structuredClone(taskStages);
 
-    if(!dragData) return
+    const { stageIndex: fromStage, taskIndex } = dragData;
 
-    const updatedStages=structuredClone(taskStages)
+    if (fromStage === stageIndex) return;
 
-    const {stageIndex:fromStage,taskIndex}=dragData
+    const [movedTask] = updatedStages[fromStage].splice(taskIndex, 1);
 
-    if(fromStage===stageIndex) return
+    updatedStages[stageIndex].push(movedTask);
 
-    const [movedTask]=updatedStages[fromStage].splice(taskIndex,1)
+    setTaskStages(updatedStages);
 
-    updatedStages[stageIndex].push(movedTask)
-
-    setTaskStages(updatedStages)
-
-    setDragData(null)
-
-
-  }
+    setDragData(null);
+  };
 
   return (
     <div className="bg-slate-400 h-[100vh]">
@@ -90,18 +105,20 @@ setDragData({stageIndex,taskIndex})
         {stages.map((stage, stageIndex) => {
           return (
             <div
-            onDragOver={allowDrop}
-            onDrop={()=>handleDrop(stageIndex)}
+              onDragOver={allowDrop}
+              onDrop={() => handleDrop(stageIndex)}
               key={stageIndex}
               className="border-2 gap-2 h-[70vh] w-[20vw] m-3"
-
             >
               <h4 className="text-center text-2xl underline mb-2">{stage}</h4>
               <ul>
                 {taskStages[stageIndex].map((task, taskIndex) => {
                   return (
-                    <li draggable key={taskIndex}
-                     onDragStart={()=>handleDragStart(stageIndex,taskIndex)}>
+                    <li
+                      draggable
+                      key={taskIndex}
+                      onDragStart={() => handleDragStart(stageIndex, taskIndex)}
+                    >
                       <div className="flex justify-center">
                         <span className="mr-2 text-lg">{task.name}</span>
                         <span className="flex justify-center">
@@ -130,7 +147,6 @@ setDragData({stageIndex,taskIndex})
           );
         })}
       </div>
-    
     </div>
   );
 }
