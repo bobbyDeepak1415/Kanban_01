87944fa76fcffd4d9dd0bd21d5a65429128f07c5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-17 03:18:35 +0530

    kanban

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import js from '@eslint/js'
+import React, { useEffect, useState } from 'react'
 
 const Demo = () => {
+
+    const [users,setUsers]=useState([])
+
+
+    useEffect(async()=>{
+
+        const response=await fetch("")
+        const json=response.json()
+        setUsers(json)
+
+    })
+
   return (
     <div>
-      
+      <h2>Hello</h2>
+      {users}
     </div>
   )
 }
