cc51c5b22c307a7bd42176aed782562901f65ac9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-16 19:47:52 +0530

    set-up done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
 
 const Demo = () => {
+
+    const [currentStatus,setCurrentStatus]=useState("")
+
+
+    const statusArr=["online","offline","unKnown","loggedOut"]
+
+
+    useEffect(()=>{
+        
+    })
+
+
     return(
+        <div>
+
         <h2>Hello</h2>
+        <p>{currentStatus}</p>
+        </div>
     )
 
     
