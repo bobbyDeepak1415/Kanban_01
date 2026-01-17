3e028cbd06c4017cc3c1c1969dfe326da09f72c5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-17 19:48:25 +0530

    set-up done

--- src/main.jsx
+++ src/main.jsx
@@ -9,39 +9,3 @@
 //   </StrictMode>,
 // )
 
-let months = {
-  Jan: 31,
-  Feb: 28,
-  Mar: 31,
-  Apr: 30,
-  May: 31,
-  Jun: 30,
-  Jul: 31,
-  Aug: 31,
-  Sep: 30,
-  Oct: 31,
-  Nov: 30,
-  Dec: 31,
-};
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-
-console.log(days);
-
-function display(month, day) {
-  let totalDays = 0;
-
-  for (let m in months) {
-    if (m === month) break;
-    totalDays += months[m];
-  }
-
-  let refIndex = 1;
-  totalDays += day - 1;
-
-  let dayIndex = (refIndex + totalDays) % 7;
-
-  return days[dayIndex];
-}
-
-// console.log(display("Jul",12))
