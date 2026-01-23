7427b0a45f09cabad68e17aba87c5dc2988a45eb
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-24 03:21:44 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,48 @@
 import { useEffect, useRef, useState } from "react";
 
 const Demo = () => {
-  return <>
-  hello
-  </>
+  const [newTask, setNewTask] = useState("");
+
+  const stages = ["Backlogs", "Todo", "Ongoing", "Done"];
+
+  const [taskStages, setTaskStages] = useState([[], [], [], []]);
+
+  return (
+    <div>
+      <div className="flex flex-col">
+        <form className="self-center">
+          <input></input>
+          <button>Add Item</button>
+        </form>
+      </div>
+
+      <div className="bg-slate-500 flex items-center justify-center">
+        {stages.map((stage, stageIndex) => {
+          return (
+            <div
+              className="border-black border-2 margin-auto m-auto h-[70vh]"
+              key={stageIndex}
+            >
+              <h1>{stage}</h1>
+
+              <ul>
+                {taskStages[stageIndex].map((taskStage, id) => {
+                  return (
+                    <li key={id}>
+                      {taskStage}
+                      <button>⬅️</button>
+                      <button>➡️</button>
+                      <button>❌</button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
