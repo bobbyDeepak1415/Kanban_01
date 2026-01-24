926b450eb0593c9b6318fc4b93263dd90e7a285e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 00:43:23 +0530

    boiler plate done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,55 +7,6 @@ const Demo = () => {
 
   const [taskStages, setTaskStages] = useState([[], [], [], []]);
 
-  const [dragData, setDragData] = useState("");
-
-  const handleAddTask = (e) => {
-    e.preventDefault();
-    if (!newTask.trim()) return;
-
-    const updatedStages = taskStages.map((taskStage) => [...taskStage]);
-
-    const task = { name: newTask.trim(), stage: 0 };
-
-    updatedStages[0].push(task);
-    setTaskStages(updatedStages);
-
-    setNewTask("");
-  };
-
-  const handleDelete = (stageIndex, taskIndex) => {
-    const updatedStages = taskStages.map((taskStage) => [...taskStage]);
-
-    updatedStages[stageIndex].splice(taskIndex, 1);
-    setTaskStages(updatedStages);
-  };
-
-  const moveTask = (stageIndex, taskIndex, direction) => {
-    const updatedStages = taskStages.map((taskStage) => [...taskStage]);
-    const taskToMove = updatedStages[stageIndex][taskIndex];
-
-    updatedStages[stageIndex + direction].push(taskToMove);
-    updatedStages[stageIndex].splice(taskIndex, 1);
-
-    setTaskStages(updatedStages);
-  };
-
-  const handleDrop = (stageIndex) => {
-    if (!dragData) return;
-
-    const { stageIndex: fromStage, taskIndex } = dragData;
-
-    const updatedStages = structuredClone(taskStages);
-
-    if (fromStage === stageIndex) return;
-
-    const [movedTask] = updatedStages[fromStage].splice(taskIndex, 1);
-
-    updatedStages[stageIndex].push(movedTask);
-    setTaskStages(updatedStages);
-    setDragData(null);
-  };
-
   return (
     <div className="bg-teal-300 h-[100vh]">
       <div className="flex justify-center h-[15vh]">
@@ -74,9 +25,6 @@ const Demo = () => {
         {stages.map((stage, stageIndex) => {
           return (
             <div
-            onDragOver={(e)=>e.preventDefault()}
-            
-              onDrop={() => handleDrop(stageIndex)}
               className="bg-slate-500 border-black border-2 margin-auto m-auto h-[70vh] w-[20vw] flex flex-col"
               key={stageIndex}
             >
@@ -87,11 +35,7 @@ const Demo = () => {
               <ul>
                 {taskStages[stageIndex].map((task, taskIndex) => {
                   return (
-                    <li
-                      onDragStart={() => setDragData({stageIndex,taskIndex})}
-                      draggable
-                      key={taskIndex}
-                    >
+                    <li key={taskIndex}>
                       <div className="flex justify-center gap-2 text-lg">
                         <span>{task.name}</span>
                         <span>
