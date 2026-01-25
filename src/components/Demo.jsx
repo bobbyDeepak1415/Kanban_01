c654010459f9e18851c2b3a6f3b539acdc107592
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 23:37:31 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,6 +7,18 @@ function Demo() {
 
   const [newTask, setNewTask] = useState("");
 
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return;
+    const updatedStages = structuredClone(taskStages);
+
+    const task = { name: newTask.trim(), stage: 0 };
+    updatedStages[0].push(task);
+
+    setTaskStages(updatedStages);
+    setNewTask("");
+  };
+
   return (
     <div className="flex flex-col bg-slate-400 h-[100vh]">
       <div className="flex justify-center">
@@ -17,15 +29,42 @@ function Demo() {
           ></input>
           <button
             className="bg-teal-300 ml-1 p-1 rounded-md"
-            // onClick={handleAddTask}
+            onClick={handleAddTask}
           >
             AddTask
           </button>
         </form>
       </div>
-      <div className="flex"></div>
+      <div className="flex">
+        {stages.map((stage, stageIndex) => {
+          return (
+            <div
+              className="items-center flex flex-col h-[80vh] border-black border-2 w-[20vw] m-auto"
+              key={stageIndex}
+            >
+              <h1 className="my-2 border-b-2 border-black">{stage}</h1>
+              <ul>
+                {taskStages[stageIndex].map((task, taskIndex) => {
+                  return (
+                    <li key={taskIndex}>
+                      <span>{task.name}</span>
+                      <span>
+                        <button>⬅️</button>
+                        <button>➡️</button>
+                        <button>❌</button>
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default Demo;
+
+// "https://jsonplaceholder.typicode.com/comments",
