b90f5f4be4b066d740c408aa4ed730d4029a3008
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 17:54:00 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,43 +7,23 @@ function Demo() {
 
   const [newTask, setNewTask] = useState("");
 
-  const handleAddTask=()=>{
-    
-  }
-
   return (
     <div className="flex flex-col bg-slate-400 h-[100vh]">
       <div className="flex justify-center">
         <form className="my-10">
-          <input value={newTask} onChange={(e)=>setNewTask(e.target.value)}></input>
-          <button onClick={handleAddTask}>AddTask</button>
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          ></input>
+          <button
+            className="bg-teal-300 ml-1 p-1 rounded-md"
+            // onClick={handleAddTask}
+          >
+            AddTask
+          </button>
         </form>
       </div>
-      <div className="flex">
-        {stages.map((stage, stageIndex) => {
-          return (
-            <div
-              className="border-black border-2 h-[80vh] w-[20vw] m-auto flex flex-col items-center"
-              key={stageIndex}
-            >
-              <h1>{stage}</h1>
-
-              {taskStages[stageIndex].map((task, taskIndex) => {
-                return (
-                  <li key={taskIndex}>
-                    <span>{task.name}</span>
-                    <span>
-                      <button>⬅️</button>
-                      <button>➡️</button>
-                      <button>❌</button>
-                    </span>
-                  </li>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+      <div className="flex"></div>
     </div>
   );
 }
