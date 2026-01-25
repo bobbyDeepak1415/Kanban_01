4c875953ab35d244e2e9bba7cbdc2e2bf679de99
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-26 01:22:50 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,7 @@
-import React, { useState } from 'react'
+import React from "react";
 
 const Demo = () => {
+  return <div></div>;
+};
 
-  const statuses=["online","offline","loggedOut","unKnown"]
-
-  const [currentSatus,setCurrentStatus]=useState("")
-
-  return (
-    <div className='h-[100vh] bg-slate-700'>
-      
-    </div>
-  )
-}
-
-export default Demo
+export default Demo;
