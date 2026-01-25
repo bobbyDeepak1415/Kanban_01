a9c7619086f6032764b7ef523e638635df4dd80f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-26 00:17:49 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,71 +1,16 @@
-import axios from "axios";
-import { comment } from "postcss";
-import React, { useEffect, useState } from "react";
-
-const PageSize = 10;
+import React, { useState } from 'react'
 
 const Demo = () => {
-  const [comments, setComments] = useState([]);
 
-  const [page, setPage] = useState(0);
+  const statuses=["online","offline","loggedOut","unKnown"]
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/comments",
-        );
-
-        setComments(response.data);
-      } catch (e) {
-        console.log("failed to fetch", e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const startIndex = page * PageSize;
-
-  const currentComments = comments.slice(startIndex, startIndex + PageSize);
-
-  const moveback = () => {
-    if (page > 0) {
-      setPage((prev) => prev - 1);
-    }
-  };
-
-  const moveForward = () => {
-    if (startIndex + PageSize <= comments) {
-      setPage((prev) => prev + 1);
-    }
-  };
+  const [currentSatus,setCurrentStatus]=useState("")
 
   return (
-    <div className="h-screen bg-slate-800">
-      <ol>
-        {currentComments.map((comment) => {
-          return <li key={comment.id}>{comment.name}</li>;
-        })}
-      </ol>
-      <div className="flex">
-        <button
-          className="bg-teal-200 px-3 py-1 m-2 rounded-md"
-          disabled={page === 0}
-          onClick={moveback}
-        >
-          Prev
-        </button>
-        <button
-          className="bg-teal-200 px-3 py-1 m-2 rounded-md"
-          onClick={moveForward}
-          disabled={startIndex + PageSize >= comments.length}
-        >
-          Next
-        </button>
-      </div>
+    <div className='h-[100vh] bg-slate-700'>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
