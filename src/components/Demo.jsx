6452d18f469564ca22012d87216e06c22d7ebb7c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-12 14:41:44 +0530

    setup done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 
 const Demo = () => {
-  
-const [count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
 
-const handleClick=()=>{
-
-}
+  const handleClick = (count, action) => {
+    switch (action.type) {
+      case "increment":
+        return count + 1;
+      case "decrement":
+        return count - 1;
+        default: return count
+    }
+  };
 
   return (
     <div>
       <p>{count}</p>
-      <button  onClick={handleClick}>+</button>
-      <button onClick={handleClick}>-</button>
+      <button
+        className="p-2 bg-slate-400"
+        onClick={() => handleClick(count, increment)}
+      >
+        +
+      </button>
+      <button
+        onClick={() => handleClick(count, decrement)}
+        className="p-2 bg-slate-500 m-2"
+        onClick={handleClick}
+      >
+        -
+      </button>
     </div>
   );
 };
